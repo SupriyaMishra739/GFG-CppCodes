b1b6799f36244cec92552e9084f9aac5eebebbd1
Supriya Mishra <91477465+SupriyaMishra739@users.noreply.github.com>
2025-03-31 03:39:17 +0530

    Time: 0 ms (100.00%) | Memory: 63.5 MB (48.63%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int size=arr.size();
+
+
+        for(int i=1;i<size;i++)
+        {
+            if( !(arr[i]>arr[i-1]))
+            {
+                return i-1;
+
+            }
+        }
+        return 0;
+        
+    }
+};

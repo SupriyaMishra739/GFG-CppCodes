7f69d6badbff6647e75fb130e8bcf3a988c5cdea
Supriya Mishra <91477465+SupriyaMishra739@users.noreply.github.com>
2025-03-30 19:58:14 +0530

    Time: 1210 ms (5.00%) | Memory: 23.9 MB (50.34%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=0;j<nums.size()-1;j++)
+            {
+                if(nums[j]==0)
+                {
+                    swap(nums[j],nums[j+1]);
+                    
+                }
+            }
+        }
+        for(int i=0;i<nums.size();i++){
+            cout<<nums[i]<<" ";
+        }
+       
+
+        
+    }
+};

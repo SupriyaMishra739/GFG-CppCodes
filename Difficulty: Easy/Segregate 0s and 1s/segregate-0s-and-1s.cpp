b594490fe87d6bc999062907fae1906550ef28c7
Supriya Mishra <91477465+SupriyaMishra739@users.noreply.github.com>
2025-03-31 04:23:51 +0530

    Update segregate-0s-and-1s.cpp

--- Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.cpp
+++ Difficulty: Easy/Segregate 0s and 1s/segregate-0s-and-1s.cpp
@@ -13,29 +13,26 @@ class Solution {
     void segregate0and1(vector<int> &arr) {
         // code here
         
+        // optimised code
+        int count0=0;
+         int count1=0;
+         
+         for(int i=0;i<arr.size();i++){
+             if(arr[i]==0){
+                 count0++;
+             }
+             else{
+                 count1++;
+             }
+         }
+         for(int i=0;i<count0;i++){
+             arr[i]=0;
+         }
+         for(int i=count0;i<count0+count1;i++){
+             arr[i]=1;
+         }
         
-        // int i=0;
-        // int j=arr.size()-1;
-        
-        // while(i<=j){
-        //     if(arr[i]!=0 && arr[j]==0){
-        //         swap(arr[i],arr[j]);
-        //         i++;
-        //         j--;
-                
-        //     }
-        //     else if(arr[j]!=0){
-        //         j--;
-        //     }
-        //     else if(arr[i]==0 && arr[j]!=0)
-        //     {
-        //         j--;
-                
-                
-        //     }
-        // }
-        
-          sort(arr.begin(), arr.end());
+        //  sort(arr.begin(), arr.end());
     }
 };
 

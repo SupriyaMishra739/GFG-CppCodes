9939de4f0c7f9bd0d90d25dd3823a80cdcd02e9b
Supriya Mishra <91477465+SupriyaMishra739@users.noreply.github.com>
2025-04-03 00:05:21 +0530

    Time: 2 ms (77.88%) | Memory: 7.7 MB (63.86%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -2,27 +2,74 @@ class Solution {
 public:
 
 
-    int fibo(int n, vector<int> &dp){
+    //Top down approach
 
+    // int fibo(int n, vector<int> &dp){
+
+    //     if(n<=1){
+    //         return n;
+    //     }
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+    //     return dp[n]= fibo(n-1,dp)+fibo(n-2,dp);
+
+
+    // }
+    // int fib(int n) {
+
+
+    //     vector<int> dp(n+1,-1);
+    //    int ans= fibo(n,dp);
+    //    return ans;
+
+
+        
+    // }
+
+
+    //Bottom up approach
+    // int fib(int n) {
+    //     if(n<=1){
+    //         return n;
+    //     }
+
+    //     vector<int> dp(n+1,-1);
+    //     dp[0]=0;
+    //     dp[1]=1;
+    //   for(int i=2;i<=n;i++){
+    //     dp[i]=dp[i-1]+dp[i-2];
+
+    //   }
+    //    return dp[n];
+
+
+        
+    // }
+
+    //optimized approach ->using only 3 variables
+      int fib(int n) {
         if(n<=1){
             return n;
         }
-        if(dp[n]!=-1){
-            return dp[n];
+       
+
+        int prev2=0, prev=1, curr;
+
+        for(int i=2;i<=n;i++){
+            curr=prev2+prev;
+            prev2=prev;
+            prev=curr;
         }
-        return dp[n]= fibo(n-1,dp)+fibo(n-2,dp);
-
-
-    }
-    int fib(int n) {
-
-
-        vector<int> dp(n+1,-1);
-       int ans= fibo(n,dp);
-       return ans;
+        return curr;
 
 
         
     }
+
+
+
+
+
    
 };

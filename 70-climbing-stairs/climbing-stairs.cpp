eda5b3a2604506feb642befae2fbbafcf2611c98
Supriya Mishra <91477465+SupriyaMishra739@users.noreply.github.com>
2025-04-03 01:30:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (17.21%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,33 +1,62 @@
 class Solution {
 public:
-    int count(int i, int n,vector<int> &dp){
-        //base case
-        if(n==i){
-            return 1;
-        }
-        if(i>n){
-            return 0;
-        }
-        if(dp[i]!=-1)
-        {
-            return dp[i];
-        }
 
-        return dp[i]=count(i+1,n,dp)+count(i+2,n,dp);
+// //Top down approach
 
-    }
+//     int count(int i, int n,vector<int> &dp){
+//         //base case
+//         if(n==i){
+//             return 1;
+//         }
+//         if(i>n){
+//             return 0;
+//         }
+//         if(dp[i]!=-1)
+//         {
+//             return dp[i];
+//         }
+
+//         return dp[i]=count(i+1,n,dp)+count(i+2,n,dp);
+
+//     }
 
 
-    int climbStairs(int n) {
+//     int climbStairs(int n) {
+//         //create a dp array
+
+//         vector<int> dp(n+2,-1);
+//       int ans=  count(0,n,dp);
+
+//       return ans;
+
+
+
+        
+//     }
+
+
+//Bottom up approach
+        int climbStairs(int n) {
         //create a dp array
+        
 
         vector<int> dp(n+2,-1);
-      int ans=  count(0,n,dp);
+        dp[n]=1;
+        dp[n+1]=0;
+        //start traversing from end
 
-      return ans;
+        for(int i=n-1;i>=0;i--){
+            dp[i]=dp[i+1]+dp[i+2];
+        }
+        return dp[0];
+     
+
+      
 
 
 
         
     }
+
+
 };

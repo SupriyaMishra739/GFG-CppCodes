1d9ff7de1ff828ec0a550458890cde77af3a536b
Supriya Mishra <91477465+SupriyaMishra739@users.noreply.github.com>
2025-04-03 10:02:19 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (71.83%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,27 +1,49 @@
 class Solution {
 public:
-    int ways(int n, vector<int> &cost, vector<int> &dp){
-        //base case
-        if(n<=1){
-            return 0;
-        }
-
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-
-        return dp[n]= min(cost[n-2]+ways(n-2,cost,dp), cost[n-1]+ways(n-1,cost,dp));
+    //Top Down Approach
 
 
-    }
+    // int ways(int n, vector<int> &cost, vector<int> &dp){
+    //     //base case
+    //     if(n<=1){
+    //         return 0;
+    //     }
+
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+
+    //     return dp[n]= min(cost[n-2]+ways(n-2,cost,dp), cost[n-1]+ways(n-1,cost,dp));
+
+
+    // }
 
 
 
-    int minCostClimbingStairs(vector<int>& cost) {
+    // int minCostClimbingStairs(vector<int>& cost) {
+    //     int n=cost.size();
+    //     vector<int> dp(n+1,-1);
+    //     int ans= ways(n,cost,dp);
+    //     return ans;
+        
+    // }
+
+
+    //Bottom Up Approach
+
+        int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
         vector<int> dp(n+1,-1);
-        int ans= ways(n,cost,dp);
-        return ans;
+        dp[0]=0;
+        dp[1]=0;
+
+        for(int i=2;i<=n;i++)
+        {
+          dp[i]= min(cost[i-2]+dp[i-2], cost[i-1]+ dp[i-1]);
+        }
+        return dp[n];
         
     }
+
+
 };

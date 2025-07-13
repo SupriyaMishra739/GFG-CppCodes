471e1c16dd54ed6a00e6d008ec033e7dd862bd9a
Supriya Mishra <91477465+SupriyaMishra739@users.noreply.github.com>
2025-07-13 23:26:56 +0530

    Time: 7 ms (67.76%) | Memory: 10.1 MB (99.54%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,26 +2,22 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
 
+        int first = 0;
+        int second = 0;
         int len=0;
-        //first and second are pointers pointing to 0th index of the string initially
-        int first=0;
-        int second=0;
+        vector<bool> count(256, 0);
 
-        vector<bool> count(256,0); //boolean array of size 256, for thr 256 possible ascii sybol initialzied with 0
+        while (second < s.length()) {
+            while (count[s[second]] == 1) {
+                count[s[first]] = 0;
 
-        while(second<s.size())
-        {
-            while(count[s[second]]==1){
-                count[s[first]]=0;
                 first++;
             }
-            count[s[second]]=1;
-
+            count[s[second]] = 1;
             len=max(len, second-first+1);
+
             second++;
         }
         return len;
-
-        
     }
 };

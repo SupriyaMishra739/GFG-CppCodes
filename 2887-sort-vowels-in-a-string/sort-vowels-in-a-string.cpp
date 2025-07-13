dec53e58e917cb258d0603a16e54d7a65953d294
Supriya Mishra <91477465+SupriyaMishra739@users.noreply.github.com>
2025-07-13 21:14:29 +0530

    Time: 499 ms (5.00%) | Memory: 15.5 MB (91.88%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    string sortVowels(string s) 
-    {
-        string vol;
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' ||
-            s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
-            {
-                vol.push_back(s[i]);
+    string sortVowels(string s) {
+        vector<char> v;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
+         
+                v.push_back(s[i]);
+                       s[i]='#';
+
             }
         }
-        sort(vol.begin(),vol.end());
-        int x=0;
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' ||
-            s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
-            {
-                s[i]=vol[x];
-                x++;
+
+        sort(v.begin(),v.end());
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='#'){
+                s[i]=v.front();
+                v.erase(v.begin());
+
             }
         }
         return s;
+
+
+        
     }
 };

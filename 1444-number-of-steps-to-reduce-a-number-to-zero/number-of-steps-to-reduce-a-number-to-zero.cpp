d55705e36962beea64074271d4ca572eb8f34092
Supriya Mishra <91477465+SupriyaMishra739@users.noreply.github.com>
2025-08-24 09:32:26 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (44.17%) - LeetSync

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int steps=0;
+
+        while(num!=0){
+            if(num%2==0){
+                //even num
+                num=num/2;
+                steps++;
+            }
+            else {
+                //num is odd
+                num=num-1;
+                steps++;
+            }
+        }
+        return steps;
+        
+    }
+};
